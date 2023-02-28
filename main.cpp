a69c82158afe533c2d6657452fd6dbe54363aea1
eastanganelli <ezequielstanganelli@gmail.com>
2023-02-28 11:26:15 -0300

    demo_v1

--- main.cpp
+++ main.cpp
@@ -1,49 +1,45 @@
 #include <iostream>
-
-// include depthai library
 #include <depthai/depthai.hpp>
-
-// include opencv library (Optional, used only for the following example)
 #include <opencv2/opencv.hpp>
+
+#define R_W 1366
+#define R_H 768
 
 using namespace std;
 
 int main(){
 
-    // Create pipeline
     dai::Pipeline pipeline;
     auto colorCam = pipeline.create<dai::node::ColorCamera>();
     auto xlinkOut = pipeline.create<dai::node::XLinkOut>();
     xlinkOut->setStreamName("preview");
     colorCam->setInterleaved(true);
-    colorCam->setPreviewSize(1920, 1080);
+    colorCam->setPreviewSize(R_W, R_H);
     colorCam->preview.link(xlinkOut->input);
 
-
     try {
-        // Try connecting to device and start the pipeline
         dai::Device device(pipeline);
 
-        // Get output queue
         auto preview = device.getOutputQueue("preview");
 
         cv::Mat frame;
         while (true) {
 
-            // Receive 'preview' frame from device
             auto imgFrame = preview->get<dai::ImgFrame>();
 
-            // Show the received 'preview' frame
-            cv::imshow("preview", cv::Mat(imgFrame->getHeight(), imgFrame->getWidth(), CV_8UC3, imgFrame->getData().data()));
+            cv::Mat input(imgFrame->getHeight(), imgFrame->getWidth(), CV_8UC3, imgFrame->getData().data());
 
-            // Wait and check if 'q' pressed
+            cv::UMat output;
+            cvtColor( input, output, cv::COLOR_BGR2GRAY );
+
+            cv::imshow("preview", output);
+
             if (cv::waitKey(1) == 'q') return 0;
 
         }
     } catch (const std::runtime_error& err) {
         std::cout << err.what() << std::endl;
     }
-
 
     return 0;
 }
